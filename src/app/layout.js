a50487060b887d8ced3ec973c7b21e4326c7cc27
mohammad_nd <mohammad0919najafi@gmail.com>
2024-06-13 19:09:53 +0330

    add header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Vazirmatn } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const vazirmatn = Vazirmatn({ subsets: ["latin"] });
 export const metadata = {
@@ -10,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={vazirmatn.className}>{children}</body>
+      <body className={vazirmatn.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
