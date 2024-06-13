041f56ff73f35a479375f4fcaa2afb331b0ed39f
mohammad_nd <mohammad0919najafi@gmail.com>
2024-06-13 19:51:55 +0330

    add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { Providers } from "./Providers";
 
 const vazirmatn = Vazirmatn({ subsets: ["latin"] });
 export const metadata = {
@@ -12,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={vazirmatn.className}>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
