800e2f1801cbbf91e2a25b63a1f9ad5895e715b9
mohammad_nd <mohammad0919najafi@gmail.com>
2024-06-14 08:30:10 +0330

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p className="">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+        necessitatibus consectetur. Doloremque exercitationem at cupiditate eos,
+        a dolor, hic repudiandae illo reiciendis illum debitis ea officia neque
+        rerum. Minus, ducimus!
+      </p>
+      <p className="">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+        necessitatibus consectetur. Doloremque exercitationem at cupiditate eos,
+        a dolor, hic repudiandae illo reiciendis illum debitis ea officia neque
+        rerum. Minus, ducimus!
+      </p>
+    </div>
+  );
+};
+
+export default About;
