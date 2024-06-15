40d1c6d183606d550a3b71cd7c105c93f7836616
mohammad_nd <mohammad0919najafi@gmail.com>
2024-06-15 22:10:04 +0330

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="flex justify-center mt-60">
+      <img src="spinner.svg" alt="loadig" className="h-52" />
+    </div>
+  );
+};
+
+export default loading;
