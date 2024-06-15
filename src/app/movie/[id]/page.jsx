90be733197546fc53452fa74034d844cc6ea9ef2
mohammad_nd <mohammad0919najafi@gmail.com>
2024-06-16 01:03:23 +0330

    create movie page

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -12,7 +12,7 @@ export default async function MoviePage({ params }) {
       <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
         <Image
           src={`https://image.tmdb.org/t/p/original/${
-            movie.backdrop_path || Mochiy_Pop_One.poster_path
+            movie.backdrop_path || movie.poster_path
           }`}
           width={500}
           height={300}
